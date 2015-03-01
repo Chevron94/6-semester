fe9c2d33b0524e3c3f11000052c36366a90557c4
Roman <chevron19941@gmail.com>
2015-03-02 00:07:14 +0400

    Add repository for new semester

---
+++ MPI/task1/task1/task1.cpp
@@ -0,0 +1,108 @@
+// task1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <mpi.h>
+#include <random>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, char* argv[])
+{
+	int m;
+	int rank;
+	int numproc;
+	int tag = 0;
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numproc);
+//	if (numproc<3) 
+	//	MPI_Abort(MPI_COMM_WORLD, 0);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+	{
+		int N=0;
+		int m_;
+		int cnt;
+		int res = 0;
+		int tmp_res;
+		cout << "Enter N\n";
+		do
+			cin >> N;
+		while (N<2);
+
+		m = N/((numproc-1)/2); // размерность кусков вектора 
+		if (N%((numproc-1)/2) != 0)
+		{
+			m_ = m+1;
+			cnt = N%((numproc-1)/2);
+		}
+		else 
+		{
+			cnt = 0;
+		}
+		for (int i=1;i<=numproc/2;i++)
+		{
+			if (cnt > 0)
+			{
+				MPI_Send(&m_, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+				MPI_Send(&m_, 1, MPI_INT, i+numproc/2, tag, MPI_COMM_WORLD);
+				cnt --;
+			}else 
+			{
+				MPI_Send(&m, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+				MPI_Send(&m, 1, MPI_INT, i+numproc/2, tag, MPI_COMM_WORLD);
+			}
+		}
+
+		for (int i=0;i<numproc/2;i++)
+		{
+			MPI_Recv(&tmp_res, 1, MPI_INT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+			res += tmp_res;
+		}
+		cout << "Result: " << res << '\n';
+	}
+	else
+	{
+		MPI_Recv(&m, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+		int *x;
+		x = new int[m];
+		srand(time(NULL));
+		char str[100]="Process: ";
+		char buf[5];
+		itoa(rank,buf,10);
+		strncat(str,buf,strlen(buf));
+		strncat(str," array: ",8);
+
+		for (int i = 0; i<m; i++)
+		{
+			x[i] = x[i]=rand()%100;
+			itoa(x[i],buf,10);
+			strncat(str,buf,strlen(buf));
+			strncat(str,", ",2);
+		}
+		cout << str << '\n';
+		if (rank<=numproc/2)
+			MPI_Send(x,m,MPI_INT,rank+numproc/2,tag,MPI_COMM_WORLD);
+		else
+		{
+			int *y;
+			y = new int[m];
+			int result = 0;
+			MPI_Recv(y,m,MPI_INT,rank-numproc/2,tag,MPI_COMM_WORLD,&status);
+			for (int i = 0; i<m; i++)
+			{
+				result+=x[i]*y[i];
+			}
+			MPI_Send(&result, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+		}
+	}
+
+	MPI_Finalize();
+	system("pause");
+	return 0;
+}
+
